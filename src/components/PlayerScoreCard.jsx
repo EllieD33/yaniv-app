41ff0f23a1af389ee33ccb50fc2af087a5750440
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-08-10 13:16:02 +0100

    implements scorecard reset button

--- src/components/PlayerScoreCard.jsx
+++ src/components/PlayerScoreCard.jsx
@@ -21,6 +21,11 @@ function PlayerScoreCard({ player, updatePlayer }) {
         const updatedPlayer = { ...player, score: newScore};
         updatePlayer(updatedPlayer);
     }
+
+    const handleResetClick = () => {
+        const updatedPlayer = { ...player, score: 0};
+        updatePlayer(updatedPlayer);
+    }
     
     return (
         <section className="py-2 flex justify-center flex-col border-2 border-indigo-600 rounded-md text-center" >
@@ -34,7 +39,7 @@ function PlayerScoreCard({ player, updatePlayer }) {
                 <button className="ml-2 bg-indigo-600 text-white rounded-full p-2" onClick={handleAsafClick} >Asaf!</button>
             </form>
             <div className="py-2">
-                <button className="ml-2 bg-red-600 text-white rounded-full p-2">Reset</button>
+                <button className="ml-2 bg-red-600 text-white rounded-full p-2" onClick={handleResetClick}>Reset</button>
             </div>
         </section>
     )
